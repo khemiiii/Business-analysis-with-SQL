cfcb17cf77a90a7ccee36185f4ae1882e3b2e4d5
khemiiii <43915225+khemiiii@users.noreply.github.com>
2021-07-23 09:18:02 +0100

    Update CapstoneProjectQuery.sql

--- CapstoneProjectQuery.sql
+++ CapstoneProjectQuery.sql
@@ -79,74 +79,74 @@ where YEARS = 2019 and MONTHS in ('october', 'november', 'december')
 group by COUNTRIES
 order by 2 desc
 
---B1.
---select TOP (3) BRANDS, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where years in ('2018', '2019')
---and COUNTRIES in ('Senegal' , 'Togo', 'Benin') 
---group by BRANDS
---order by 2 desc
+--Listing the top 3 brands consumed in the francophone countries over the last 2 years
+select TOP (3) BRANDS, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where years in ('2018', '2019')
+and COUNTRIES in ('Senegal' , 'Togo', 'Benin') 
+group by BRANDS
+order by 2 desc
 
---B2.
---select top (2) BRANDS, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where COUNTRIES = 'Ghana'
---group by BRANDS
---order by 2 desc
+--Pinning down the top 2 in demand consumer brands in ghana
+select top (2) BRANDS, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where COUNTRIES = 'Ghana'
+group by BRANDS
+order by 2 desc
 
---B3.
---select BRANDS, UNIT_PRICE, sum(quantity) as quantity, sum(cost) as cost, sum(profit) as profit, REGION, MONTHS, YEARS
---from International_Breweries$
---where COUNTRIES = 'Nigeria'
---and brands not like '%malt'
---group by brands, UNIT_PRICE, region, YEARS, MONTHS
+--Extracting details of beers consumed in Nigeria over the past 3 years
+select BRANDS, UNIT_PRICE, sum(quantity) as quantity, sum(cost) as cost, sum(profit) as profit, REGION, MONTHS, YEARS
+from International_Breweries$
+where COUNTRIES = 'Nigeria'
+and brands not like '%malt'
+group by brands, UNIT_PRICE, region, YEARS, MONTHS
 
 
---B4.
---select TOP (1) BRANDS, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where years in ('2018', '2019')
---and COUNTRIES in ('Nigeria', 'Ghana') 
---and BRANDS like '%malt'
---group by BRANDS
---order by 2 desc
+--Figuring out the favorite malt brand in the anglophone region between the years 2018 and 2019
+select TOP (1) BRANDS, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where years in ('2018', '2019')
+and COUNTRIES in ('Nigeria', 'Ghana') 
+and BRANDS like '%malt'
+group by BRANDS
+order by 2 desc
 
---B5.
---select BRANDS, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where years = '2019'
---and COUNTRIES in ('Nigeria') 
---group by BRANDS
---order by 2 desc
+--Brands with highest sales in 2019 in Nigeria
+select BRANDS, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where years = '2019'
+and COUNTRIES in ('Nigeria') 
+group by BRANDS
+order by 2 desc
 
---B6.
---select top (1) BRANDS, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where REGION = 'southsouth'
---and COUNTRIES in ('Nigeria') 
---group by BRANDS
---order by 2 desc
+--The favorite brand in the South-South region in Nigeria 
+select top (1) BRANDS, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where REGION = 'southsouth'
+and COUNTRIES in ('Nigeria') 
+group by BRANDS
+order by 2 desc
 
-----B7.
---select BRANDS, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where COUNTRIES = 'Nigeria'
---and brands not like '%malt'
---group by brands
+--The consumption rate of beer in Nigeria 
+select BRANDS, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where COUNTRIES = 'Nigeria'
+and brands not like '%malt'
+group by brands
 
-----B8.
---select BRANDS, region, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where COUNTRIES = 'Nigeria'
---and brands like 'budweiser'
---group by region, brands
+--Level of consumption of the budweiser brand in the different regions in Nigeria 
+select BRANDS, region, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where COUNTRIES = 'Nigeria'
+and brands like 'budweiser'
+group by region, brands
+order by 3 desc
+
+--Analyzing the level of consumption of the budweiser brand in the different regions in Nigeria in the year 2019 inorder to make a decision on a promo plan
+select BRANDS, region, sum(QUANTITY) as TotalQuantity
+from International_Breweries$
+where COUNTRIES = 'Nigeria'
+and YEARS = 2019
+and brands like 'budweiser'
+group by region, brands
 --order by 3 desc
-
-----B9.
---select BRANDS, region, sum(QUANTITY) as TotalQuantity
---from International_Breweries$
---where COUNTRIES = 'Nigeria'
---and YEARS = 2019
---and brands like 'budweiser'
---group by region, brands
-----order by 3 desc
